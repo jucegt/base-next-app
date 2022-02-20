ba88613d25ba63ab6ef898bb68690dd4274a2cac
jucegt <julio2201@gmail.com>
2022-02-19 18:11:51 -0600

    Changed Next.js to 12.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,29 @@
 module.exports = {
-  'env': {
-    'es2021': true,
-    'node': true
+  env: {
+    es2021: true,
+    node: true,
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@next/next/recommended',
   ],
-  'settings': {
-    'react': {
-      'version': 'detect'
-    }
-  },
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
+  settings: {
+    react: {
+      version: 'detect',
     },
-    'ecmaVersion': 12,
-    'sourceType': 'module'
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint'
-  ],
-  'rules': {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
     'react/react-in-jsx-scope': 0,
     'react/display-name': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
@@ -37,7 +35,7 @@ module.exports = {
     '@typescript-eslint/no-use-before-define': 0,
     '@typescript-eslint/ban-ts-ignore': ['off'],
     '@typescript-eslint/no-unescaped-entities': 0,
-    '@typescript-eslint/no-unused-vars': [2, { 'argsIgnorePattern': '^_' }],
+    '@typescript-eslint/no-unused-vars': [2, { argsIgnorePattern: '^_' }],
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
     'react/no-unescaped-entities': 0,
@@ -45,16 +43,16 @@ module.exports = {
     'no-empty-pattern': ['off'],
     'no-undef': ['error'],
     'no-var': ['error'],
-    'no-console': [2, { 'allow': ['warn', 'error'] }],
+    'no-console': [2, { allow: ['warn', 'error'] }],
     'object-curly-spacing': ['error', 'always'],
     'indent': ['error', 2],
     'quotes': ['error', 'single'],
-    'semi': ['error', 'always']
+    'semi': ['error', 'always'],
   },
-  'overrides': [
+  overrides: [
     {
-      'files': ['src/**/*.ts', 'src/**/*.tsx'],
-      'excludedFiles': ['next-env.d.ts']
-    }
-  ]
+      files: ['src/**/*.ts', 'src/**/*.tsx'],
+      excludedFiles: ['next-env.d.ts'],
+    },
+  ],
 };
